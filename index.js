7ca1eaa20a3c783f2b4c61ae198b9127e67341ca
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2024-03-24 19:40:54 +0530

    log

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/",(req,res)=>{
-    res.send("Home Page")
-})
+// app.use("/",(req,res)=>{
+//     res.send("Home Page")
+// })
 
 
 
